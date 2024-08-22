dcead856dcaadd32278e8581c4fd483c9aaafe4f
Dinesh_ramisetti <ramisettidinesh533@gmail.com>
2024-08-22 10:34:51 +0530

    employee

---
+++ src/main/java/com/neoteric/fullstack/function/Employee.java
@@ -0,0 +1,26 @@
+package com.neoteric.fullstack.function;
+
+public class Employee {
+
+
+        private String firstName;
+        private String lastName;
+
+        public Employee (String firstName,String lastName){
+            this.firstName=firstName;
+            this.lastName=lastName;
+
+        }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+
+    }
+
+}
+
+
